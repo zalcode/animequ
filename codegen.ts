16670343eef75675989bc23412bb9b3a4122a334
Muhammad Rizal <rizal@ruangguru.com>
2025-05-21 00:14:24 +0700

    chore: update dependencies and add GraphQL code generation

---
+++ codegen.ts
@@ -0,0 +1,29 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://graphql.anilist.co',
+  documents: ['src/**/*.tsx', 'src/**/*.ts'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    'src/gql/': {
+      preset: 'client',
+      plugins: [],
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+    './schema.graphql': {
+      plugins: ['schema-ast'],
+      config: {
+        includeDirectives: true,
+      },
+    },
+  },
+  hooks: {
+    afterAllFileWrite: ['prettier --write'],
+  },
+  watch: true,
+};
+
+export default config;
